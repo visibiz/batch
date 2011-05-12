73168f5c7ad5cb921842290af66fd6f8043c0819
Mike Brocious <mike@visibiz.com>
2011-05-12 19:39:10 -0400

    Scaffolded app

---
+++ grails-app/jobs/com/visibiz/jobs/AppStatsJob.groovy
@@ -0,0 +1,23 @@
+package com.visibiz.jobs
+
+
+class AppStatsJob {
+    
+    // WHAT DOES THIS TIMEOUT DO????
+    def timeout = 5000l // execute job once in 5 seconds
+    
+    // Don't need Hibernate session
+    def sessionRequired = false
+    
+    static triggers = {
+        // Repeat every hour
+        simple name:'repeatEveryFiveSeconds', startDelay:10000, repeatInterval: 5000
+//        cron name:'cronTrigger', startDelay:10000, cronExpression: '0/6 * 15 * * ?'
+//        custom name:'customTrigger', triggerClass:MyTriggerClass, myParam:myValue, myAnotherParam:myAnotherValue
+    }
+ 
+    def execute(context) {
+        println("${new Date()}: AppStats")
+//        println("${new Date()}: AppStats - context: $context")
+    }
+}
---
+++ test/unit/batch/AppStatsJobTests.groovy
@@ -0,0 +1,17 @@
+package batch
+
+import grails.test.*
+
+class AppStatsJobTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
